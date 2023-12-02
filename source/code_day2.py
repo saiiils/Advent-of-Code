e0b171fc9926e567cf62abfc6a5656467cd5b7ca
saiiils <matthew.kim64@gmail.com>
2023-12-02 00:25:43 -0800

    day 2, part one complete

--- source/code_day2.py
+++ source/code_day2.py
@@ -6,3 +6,50 @@ import re
 file = open("inputs/input_day2.txt", "r")
 data = file.readlines()
 file.close()
+
+# the bag of cubes to compare to
+maxRed = 12
+maxGreen = 13
+maxBlue = 14
+
+# total sum of IDs
+sumID = 0
+
+# part one
+for line in data:
+    gameValid = True
+    gameID = int(re.search(r'\d+', line.split(':')[0]).group())
+    print("Game: ", gameID)
+    gameData = line.split(':')[1]
+    setData = gameData.split(';')
+    for set in setData:
+        print("Set: ", set)
+        colorData = set.split(',')
+        for cube in colorData:
+            # print(cube)
+            cubeColor = re.search(r'red|green|blue', cube).group()
+            if (cubeColor == 'red'):
+                cubeNum = int(re.search(r'\d+', cube).group())
+                if (cubeNum > maxRed):
+                    gameValid = False
+                    # print("game invalid. red: ", cubeNum, " exceeds max red: ", maxRed)
+                    break
+            if (cubeColor == 'green'):
+                cubeNum = int(re.search(r'\d+', cube).group())
+                if (cubeNum > maxGreen):
+                    gameValid = False
+                    # print("game invalid. green: ", cubeNum, " exceeds max green: ", maxGreen)
+                    break
+            if (cubeColor == 'blue'):
+                cubeNum = int(re.search(r'\d+', cube).group())
+                if (cubeNum > maxBlue):
+                    gameValid = False
+                    # print("game invalid. blue: ", cubeNum, " exceeds max blue: ", maxBlue)
+                    break
+        if not gameValid:
+            # print("break out early")
+            break
+    if gameValid:
+        sumID = sumID + gameID
+        # print("this game is valid, the ID has been added to the sum...")
+    print(sumID)
