45763497c0791d947fe61eca529c6b14673cc73e
saiiils <matthew.kim64@gmail.com>
2024-12-02 08:59:53 -0800

    2024 yayyy

---
+++ 2024/source/code_day1.py
@@ -0,0 +1,30 @@
+# Advent of Code 2024
+# Day 1: Historian Hysteria
+
+import re
+
+file = open("inputs/input_day1", "r")
+data = file.readlines()
+file.close()
+
+firstNums = []
+secondNums = []
+diffNums = []
+simScore = 0
+
+for line in data:
+    numbers = [int(word) for word in line.split() if word.isdigit()]
+    firstNums.append(numbers[0])
+    secondNums.append(numbers[1])
+
+for num in firstNums:
+    simScore += num * secondNums.count(num)
+
+firstNums.sort()
+secondNums.sort()
+
+for first, second in zip(firstNums, secondNums):
+    diffNums.append(abs(first - second))
+
+print("Total distance: ", sum(diffNums))
+print("Similarity score: ", simScore)
---
+++ 2024/source/code_day2.py
@@ -0,0 +1,81 @@
+# Advent of Code 2024
+# Day 2: Red-Nosed Reports
+
+import re
+import numpy
+
+file = open("inputs/input_day2", "r")
+data = file.readlines()
+file.close()
+
+safeReports = 0
+
+def checkOrder(nums):
+    sortNums = nums[:]
+    sortNums.sort()
+    revSortNums = sortNums[::-1]
+
+    if nums == sortNums:
+        # print("forward: ", nums)
+        return True
+    elif nums == revSortNums:
+        # print("reverse: ", nums)
+        return True
+    else:
+        # print("not sorted")
+        return False
+
+def checkDiff(nums):
+    diffs = numpy.diff(nums)
+    # print(diffs)
+    safety = False
+    for i in diffs:
+        if 1 <= abs(i) <= 3:
+            # print("good diffs: ", diffs)
+            # print(i)
+            safety = True
+        else:
+            safety = False
+            break
+    # if not safety:
+    #     print("bad diffs: ", diffs)
+
+    return safety
+
+def sublists(nums):
+    n = len(nums)
+    sublists = []
+
+    for i in range(n):
+        appendList = nums[:]
+        appendList.pop(i)
+        sublists.append(appendList)
+
+    # print(sublists)
+    return sublists
+
+def checkSafety(nums):
+    return checkOrder(nums) and checkDiff(nums)
+
+def problemDampener(subLists):
+    print("hello")
+
+for line in data:
+    numbers = [int(word) for word in line.split() if word.isdigit()]
+    sublist = sublists(numbers)
+
+    subSafety = False
+    for sub in sublist:
+        if checkSafety(sub):
+            subSafety = True
+
+    if checkSafety(numbers) or subSafety:
+        # print("safe report")
+        safeReports += 1
+    # else:
+    #     print("unsafe report")
+    #     None
+
+    # print("\n")
+
+print(safeReports)
