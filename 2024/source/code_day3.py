33384a6d2d52c63ec9164c709f6ed1d2786abb80
saiiils <matthew.kim64@gmail.com>
2024-12-03 18:43:12 -0800

    day 3 doneeeee

--- 2024/source/code_day3.py
+++ 2024/source/code_day3.py
@@ -7,12 +7,34 @@ import re
 
 file = open("2024/inputs/input_day3", "r")
 
-pattern = r'\bmul\([0-9]+,[0-9]+\)\b'
-mults = re.findall(pattern, file.read())
+pattern = r'mul\([0-9]+,[0-9]+\)'
+
+mults = re.split("(do\(\)|don't\(\))", file.read())
 
 file.close()
 
-print(mults)
+multEnable = True
+sum = 0
 
-for pair in mults:
+for split in mults:
+
+    if split == "do()":
+        multEnable = True
+    elif split == "don't()":
+        multEnable = False
+    else:
+        print("doing math")
+
+    if multEnable:
+        mults = re.findall(pattern, split)
+        for mul in mults:
+            numbers = list(map(int, re.findall(r'\d+', mul)))
+            sum += numbers[0] * numbers[1]
+
+# mults = re.findall(pattern, file.read())
+
+# for pair in mults:
+#     numbers = list(map(int, re.findall(r'\d+', pair)))
+#     sum += numbers[0] * numbers[1]
     
+print(sum)
